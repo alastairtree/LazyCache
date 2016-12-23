a0d68e17974bc5692e69edc6ba0234f4b62a404d
Alastair Crabtree <alastaircrabtree@gmail.com>
2016-12-23 23:45:10 +0000

    Rev version

--- LazyCache/Properties/AssemblyInfo.cs
+++ LazyCache/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.7.0.*")]
+[assembly: AssemblyVersion("0.7.1.*")]
