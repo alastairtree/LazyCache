55d383bfea8ef1558b65ae5b28eeb8ebbf88a2ea
Alastair Crabtree <alastaircrabtree@gmail.com>
2018-02-27 22:27:05 +0000

    test: send unit tests results to appveyor

--- build.ps1
+++ build.ps1
@@ -29,18 +29,33 @@ function Exec
 $config = "release"
 
 Try {
-	Exec { dotnet restore }
 
+	# Get dependencies from nuget and compile
+	Exec { dotnet restore }
 	Exec { dotnet build --configuration $config --no-restore }
 
-	Get-ChildItem .\**\*.csproj -Recurse |  Where-Object { $_.Name -match ".*Test(s)?.csproj$"} | ForEach-Object { 
-		Exec { dotnet test $_.FullName --configuration $config --no-build --no-restore }
+	# Find each test project and run tests. upload results to AppVeyor
+	Get-ChildItem .\**\*.csproj -Recurse | 
+		Where-Object { $_.Name -match ".*Test(s)?.csproj$"} | 
+		ForEach-Object { 
+		
+			Exec { dotnet test $_.FullName --configuration $config --no-build --no-restore --logger "trx;LogFileName=..\..\test-result.trx" }
+	
+			# if on build server upload results to AppVeyor
+			if ("${ENV:APPVEYOR_JOB_ID}" -ne "") {
+				$wc = New-Object 'System.Net.WebClient'
+				$wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\test-result.trx)) 
+			}
+
+			Remove-Item .\test-result.trx -ErrorAction SilentlyContinue
 	}
 
+	# Publish the nupkg artifacts
 	if (Get-Command "Push-AppveyorArtifact" -errorAction SilentlyContinue)
 	{
 		Get-ChildItem .\*\bin\$config\*.nupkg -Recurse | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
 	}
+
 } Catch {
 	$host.SetShouldExit(-1)
 	throw
