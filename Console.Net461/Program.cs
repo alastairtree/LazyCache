ecb0d74f94f504ef9aa9e139842837e912dacbe2
Alastair Crabtree <alastair.crabtree@bbc.com>
2019-02-23 13:07:09 +0000

    feat: add a net461 sample

---
+++ Console.Net461/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LazyCache;
+
+namespace Console.Net461
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAppCache cache = new CachingService(CachingService.DefaultCacheProvider);
+
+            var item = cache.GetOrAdd("Program.Main.Person", () => Tuple.Create("Joe Blogs", DateTime.UtcNow));
+
+            System.Console.WriteLine(item.Item1);
+        }
+    }
+}
