dbc06eda7dd2dac0ae51faa6b7fb69c723dcd24f
adobarganes1 <adobarganes1@CND62054V6>
2019-03-22 15:48:19 -0400

    added distributed cache extensions

---
+++ LazyCache.AspNetCore/DistributedLazyCacheServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using LazyCache;
+using LazyCache.Providers;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+// ReSharper disable once CheckNamespace - MS guidelines say put DI registration in this NS
+namespace Microsoft.Extensions.DependencyInjection
+{
+    // See https://github.com/aspnet/Caching/blob/dev/src/Microsoft.Extensions.Caching.Memory/MemoryCacheServiceCollectionExtensions.cs
+    public static class DistributedLazyCacheServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDistributedLazyCache(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddOptions();
+            services.TryAdd(ServiceDescriptor.Singleton<IDistributedCache, MemoryDistributedCache>());
+            services.TryAdd(ServiceDescriptor.Singleton<IDistributedCacheProvider, DistributedCacheProvider>());
+
+            services.TryAdd(ServiceDescriptor.Singleton<IAppCache, CachingService>());
+
+            return services;
+        }
+
+        public static IServiceCollection AddDistributedLazyCache(this IServiceCollection services,
+            Func<IServiceProvider, DistributedCachingService> implementationFactory)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (implementationFactory == null) throw new ArgumentNullException(nameof(implementationFactory));
+
+            services.AddOptions();
+            services.TryAdd(ServiceDescriptor.Singleton<IDistributedCacheProvider, DistributedCacheProvider>());
+
+            services.TryAdd(ServiceDescriptor.Singleton<IDistributedAppCache>(implementationFactory));
+
+            return services;
+        }
+    }
+}
