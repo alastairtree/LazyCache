e8fbd4b6d857e5bd51613d8450a98d5c989e173f
Jeremy Oursler <jeremy@rubberchickenparadise.com>
2019-06-19 20:00:59 -0500

    Fixes #71 Updated version reference of Microsoft.Extensions.Caching.Memory to 2.1.2 to match Microsoft.AspNetCore.App 2.1 version restrictions Added additional web sample project targeting .net core 2.1 (Long Term Support version) Removed unnecessary package references because of dependent packages Ensured that an exe referencing Microsoft.Extensions.Caching.Memory --version 2.2.0 will execute correctly Executed unit tests to ensure passing

---
+++ CacheDatabaseQueriesApiSample.netCore2.1LTS/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CacheDatabaseQueriesApiSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+        }
+    }
+}
