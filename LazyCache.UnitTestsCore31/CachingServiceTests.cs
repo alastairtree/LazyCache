12442877e158e7d2a1b2696401da61a8ac132bbd
Alastair Crabtree <alastair.crabtree@bbc.com>
2019-12-27 22:47:12 +0000

    task: reintroduce support for core 2.1 as it is LTS
    * downgrade references to core framework 2.1 (not 2.2) since that is the current LTS release
    * still allows nuget and child projects to reference newer versions of the dependencies as shown in the tests
    * Add tests for 2.1, 2.2, 3.0 and 3.1 frameworks and test on every build
    * fix compiler warning about nuget package logo
    
    fixes #71

---
+++ LazyCache.UnitTestsCore31/CachingServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using LazyCache.Providers;
+using Microsoft.Extensions.Caching.Memory;
+using NUnit.Framework;
+
+namespace LazyCache.UnitTestsCore31
+{
+    [TestFixture]
+    public class CachingServiceTests
+    {
+        private static CachingService BuildCache()
+        {
+            return new CachingService(new MemoryCacheProvider(new MemoryCache(new MemoryCacheOptions())));
+        }
+
+        private IAppCache sut;
+
+
+        private const string TestKey = "testKey";
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            sut = BuildCache();
+        }
+
+
+        [Test]
+        public void GetOrAddOnCore31ReturnsTheCachedItem()
+        {
+            var cachedResult = sut.GetOrAdd(TestKey, () => new {SomeProperty = "SomeValue"});
+
+            Assert.IsNotNull(cachedResult);
+            Assert.AreEqual("SomeValue", cachedResult.SomeProperty);
+        }
+    }
+}
