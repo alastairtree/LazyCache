bba7f54057995c70272ce0830cab3065c58ce371
matthew-p <matthewpische@gmail.com>
2020-09-27 08:10:10 -0400

    Issue72 Deprecate Add<T> method in favor of Set<T>

---
+++ LazyCache.UnitTests/DeprecationTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace LazyCache.UnitTests
+{
+    public class DeprecationTests
+    {
+        [Test]
+        public void AllImplementationsOfIAppCache_HaveTheAddMethod_MarkedAsObsolete()
+        {
+            var classesWithoutObsoleteTag = new List<Type>();
+
+            foreach (var type in GetTypesWithIAppCache())
+            {
+                if (MethodDoesNotHaveObsoleteAttribute(type))
+                {
+                    classesWithoutObsoleteTag.Add(type);
+                }
+            }
+            CollectionAssert.IsEmpty(classesWithoutObsoleteTag);
+        }
+
+        private IEnumerable<Type> GetTypesWithIAppCache()
+        {
+            var iAppCache = typeof(IAppCache);
+            return Assembly.GetAssembly(iAppCache)
+                .GetTypes()
+                .Where(iAppCache.IsAssignableFrom);
+        }
+
+        private bool MethodDoesNotHaveObsoleteAttribute(Type type)
+        {
+            var method = type.GetMethods()
+                .SingleOrDefault(m => m.Name == nameof(IAppCache.Add));
+            var attribute = method?.GetCustomAttributes(typeof(ObsoleteAttribute), true)
+                .SingleOrDefault() as ObsoleteAttribute;
+            return attribute == null;
+        }
+
+    }
+}
--- LazyCache/IAppCache.cs
+++ LazyCache/IAppCache.cs
@@ -12,6 +12,7 @@ namespace LazyCache
         ///     Define the number of seconds to cache objects for by default
         /// </summary>
         CacheDefaults DefaultCachePolicy { get; }
+        [Obsolete("This method has been deprecated. Use Set<T> instead.", false)]
         void Add<T>(string key, T item, MemoryCacheEntryOptions policy);
         T Get<T>(string key);
         Task<T> GetAsync<T>(string key);
@@ -20,5 +21,6 @@ namespace LazyCache
         Task<T> GetOrAddAsync<T>(string key, Func<ICacheEntry, Task<T>> addItemFactory);
         Task<T> GetOrAddAsync<T>(string key, Func<ICacheEntry, Task<T>> addItemFactory, MemoryCacheEntryOptions policy);
         void Remove(string key);
+        void Set<T>(string key, T item, MemoryCacheEntryOptions policy);
     }
 }
