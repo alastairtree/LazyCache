03b7a1799003f886519d4ea67cd9d5c59158fedc
Jonathan King <jonathan.king@healthcatalyst.com>
2021-05-07 16:54:44 -0700

    add dictionary to maintain cache keys

---
+++ LazyCache/CachedItemMeta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LazyCache
+{
+    public class CachedItemMeta
+    {
+        public CachedItemMeta()
+        {
+            this.CreatedDate = DateTime.UtcNow;
+        }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
